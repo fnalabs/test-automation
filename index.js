fd6174acfd23b3032377687dae88cee5dfdc26cd
Adam Eilers <adam.eilers@gmail.com>
2017-03-22 22:41:35 -0700

    fixing incorrect path to classes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('babel-polyfill');
 
 // expose library
-exports.Fragment = require('./dist/Fragment');
-exports.Sequence = require('./dist/Sequence');
+exports.Fragment = require('./dist/js/Fragment');
+exports.Sequence = require('./dist/js/Sequence');
